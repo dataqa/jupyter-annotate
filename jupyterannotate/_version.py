2d14d11aea0b7ecf23698d12403ccfab9c42f70e
Stuart Quin <stuart.quin@gmail.com>
2022-08-10 21:19:46 +0100

    Package.json single source of version

--- jupyterannotate/_version.py
+++ jupyterannotate/_version.py
@@ -4,5 +4,16 @@
 # Copyright (c) Stuart Quin.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 2)
-__version__ = ".".join(map(str, version_info))
+import json
+from os.path import exists
+
+if exists("./package.json"):
+    package_json = None
+    with open("./package.json", "r") as package_json_file:
+        package_json = json.loads(package_json_file.read())
+
+    __version__ = package_json["version"]
+else:
+    __version__ = "0.0.0"
+
+version_info = tuple(__version__.split("."))
